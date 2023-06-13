b5d5007c6ae3e90cc69a49dbb79a0b32dc0b04da
dzksh <dakshdhangar@gmail.com>
2023-06-13 22:08:54 +0530

    one_more

--- api/index.js
+++ api/index.js
@@ -23,12 +23,12 @@ const bucket = 'daksh-booking-app';
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
-
 app.use(cors({
   credentials: true,
   origin: process.env.VERCEL_URL,
 }));
 console.log(process.env.VERCEL_URL)
+
 async function uploadToS3(path, originalFilename, mimetype) {
   const client = new S3Client({
     region: 'ap-south-1',
